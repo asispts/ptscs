41763aef3692ba52177feefd48ee63d2b7938dfb
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2022-08-02 01:04:10 +0900

    Format namespace and uses

--- tests/Sniffs/Squiz/ClassTest.php
+++ tests/Sniffs/Squiz/ClassTest.php
@@ -10,18 +10,21 @@ final class ClassTest extends SniffTestCase
 {
     protected function setUp(): void
     {
-        $this->appendExclude('Squiz.Classes.ClassFileName');
+        $this->setExclude(['Squiz.Classes.ClassFileName']);
     }
 
     public function provideTestData(): Iterator
     {
         yield[
           [
-            new ErrorData(7, 'PSR2.Classes.ClassDeclaration.ExtendsLine'),
-            new ErrorData(8, 'PSR2.Classes.ClassDeclaration.ImplementsLine'),
-            new ErrorData(10, 'PSR12.Traits.UseDeclaration.NoBlankLineAfterUse'),
-            new ErrorData(13, 'Squiz.WhiteSpace.MemberVarSpacing.Incorrect'),
-            new ErrorData(14, 'Squiz.WhiteSpace.FunctionSpacing.Before'),
+            new ErrorData(4, 'SlevomatCodingStandard.TypeHints.DeclareStrictTypes'),
+            new ErrorData(5, 'SlevomatCodingStandard.Namespaces.NamespaceSpacing'),
+            new ErrorData(7, 'SlevomatCodingStandard.Namespaces.UseSpacing'),
+            new ErrorData(9, 'PSR2.Classes.ClassDeclaration.ExtendsLine'),
+            new ErrorData(10, 'PSR2.Classes.ClassDeclaration.ImplementsLine'),
+            new ErrorData(12, 'PSR12.Traits.UseDeclaration.NoBlankLineAfterUse'),
+            new ErrorData(15, 'Squiz.WhiteSpace.MemberVarSpacing.Incorrect'),
+            new ErrorData(16, 'Squiz.WhiteSpace.FunctionSpacing.Before'),
           ],
         ];
     }
