4f12b5b02c1a6484acad4fbc0fc22dbf09666e38
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2022-08-02 14:51:58 +0900

    Add PSR12 file header order issue

---
+++ tests/Sniffs/Unsolved/Psr12FileHeaderTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Ptscs\Tests\Sniffs\Unsolved;
+
+use Iterator;
+use Ptscs\Tests\SniffTestCase;
+use Ptscs\Tests\Utils\ErrorData;
+
+final class Psr12FileHeaderTest extends SniffTestCase
+{
+    public function provideTestData(): Iterator
+    {
+        yield[
+          [
+            new ErrorData(3, 'PSR12.Files.FileHeader.IncorrectOrder'),
+          ],
+        ];
+    }
+}
