81553b971b4a550ce708bd88fa05f5f5682b90bc
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2022-08-11 19:32:31 +0900

    Detect useless variable

--- README.md
+++ README.md
@@ -257,6 +257,7 @@ $arr = array(); // not allowedgit a
 31. Disallow `use` from same namespace
 32. Detect useless alias
 33. Reference used name only (`SlevomatCodingStandard.Namespaces.ReferenceUsedNamesOnly`)
+34. Detect useless variable declaration
 
 ## License
 Released under [MIT License](https://opensource.org/licenses/MIT).
---
+++ tests/Sniffs/Slevomat/UselessVariableTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Ptscs\Tests\Sniffs\Slevomat;
+
+use Iterator;
+use Ptscs\Tests\SniffTestCase;
+use Ptscs\Tests\Utils\ErrorData;
+
+final class UselessVariableTest extends SniffTestCase
+{
+    public function provideTestData(): Iterator
+    {
+        yield[
+          [
+            new ErrorData(5, 'SlevomatCodingStandard.Variables.UselessVariable'),
+          ],
+        ];
+    }
+}
