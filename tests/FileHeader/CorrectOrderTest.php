4aefafb7770f84e1540dbe03826f2a9a4f72311e
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2023-05-09 20:44:56 +0900

    Fix PSR12 FileHeader incorrect order

---
+++ tests/FileHeader/CorrectOrderTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Ptscs\Tests\FileHeader;
+
+use Iterator;
+use Ptscs\Tests\SniffTestCase;
+
+final class CorrectOrderTest extends SniffTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setExclude(['Squiz.Classes.ClassFileName.NoMatch']);
+    }
+
+    public static function provideTestData(): Iterator
+    {
+        yield[];
+    }
+}
