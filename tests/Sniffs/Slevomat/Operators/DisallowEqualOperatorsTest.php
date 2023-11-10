27180c74ca252c14244e5e5eb90e3f6edaefb971
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2023-11-10 19:40:26 +0700

    Disallow loose comparison operator (#21)

---
+++ tests/Sniffs/Slevomat/Operators/DisallowEqualOperatorsTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Ptscs\Tests\Sniffs\Slevomat\Operators;
+
+use Iterator;
+use Ptscs\Tests\SniffTestCase;
+use Ptscs\Tests\Utils\ErrorData;
+
+final class DisallowEqualOperatorsTest extends SniffTestCase
+{
+    public static function provideTestData(): Iterator
+    {
+        yield[
+          [
+            new ErrorData(5, 'SlevomatCodingStandard.Operators.DisallowEqualOperators.DisallowedEqualOperator'),
+          ],
+        ];
+    }
+}
